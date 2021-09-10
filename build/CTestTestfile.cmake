7bbbfcda5ff62e4a29691ff0f86c2cd42ab5c466
aahladv <33842800+aahladv@users.noreply.github.com>
2021-09-10 16:19:36 -0400

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/aahlad/catkin_ws/src
+# Build directory: /home/aahlad/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
+subdirs("sonar_files")
+subdirs("rplidar_ros")
